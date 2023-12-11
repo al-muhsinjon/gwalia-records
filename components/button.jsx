ec942a8838016a03b9414cf4386db38a9f4fd6c8
Mullajonov Muhsinjon <122345056+al-muhsinjon@users.noreply.github.com>
2023-12-11 16:57:15 +0500

    finishmi

--- components/button.jsx
+++ components/button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ children }) => {
   return (
-    <button className="px-5 font-bold py-2 relative before:absolute before:content-[''] text-white before:top-0 before:left-0 overflow-hidden before:w-full before:h-0 before:bg-slate-400  rounded-md border-2 z-[1] before:-z-[1] hover:before:h-full before:transition-[5s] bg-red-600">
+    <button className="px-5 font-bold py-2 relative before:absolute before:content-[''] text-white before:top-0 before:left-0 overflow-hidden before:w-full before:h-0 before:bg-slate-400  rounded-md border-2 border-red-600 z-[1] before:-z-[1] hover:before:h-full before:transition-[5s] bg-red-600">
       {children}
     </button>
   );
--- components/packages.jsx
+++ components/packages.jsx
@@ -13,6 +13,33 @@ const Packages = () => {
       <div className="w-full h-auto  flex flex-col md:flex-row gap-y-4 gap-8 p-6 justify-center items-center">
         <div className="w-full h-full border-2 border-red-600 rounded-md   bg-black p-8 text-white text-center">
           <div className="flex text-4xl py-3 gap-4 flex-col justify-center items-center">
+            <ImHeadphones />
+            <h2>Debyut</h2>
+          </div>
+          <div className="flex items-center justify-between py-4">
+            <FaCheck className="text-red-600" />
+            <p>Yiliga 2 ta albom</p>
+          </div>
+          <hr />
+          <div className="flex items-center justify-between py-4 ">
+            <FaCheck className="text-red-600" />
+            <p>1 x ustuvor konsert chiptasi</p>
+          </div>
+          <hr />
+          <div className="flex items-center justify-between py-4 ">
+            <FaCheck className="text-red-600" />
+            <p>1 x bepul tovar mahsuloti</p>
+          </div>
+          <hr />
+          <div className="flex items-center justify-between py-4">
+            <FaCheck className="text-red-600" />
+            <p>Qo'shimcha albom xaridlariga 10% chegirma.</p>
+          </div>
+          <Button>Buy Package</Button>
+        </div>
+
+        <div className="w-full h-full border-2 border-red-600 rounded-md   bg-black p-8 text-white text-center">
+          <div className="flex text-4xl py-3 gap-4 flex-col justify-center items-center">
             <BiAlbum />
             <h2>Upgrade</h2>
           </div>
@@ -60,34 +87,7 @@ const Packages = () => {
           <hr />
           <div className="flex items-center justify-between py-4">
             <FaCheck className="text-red-600" />
-            <p>Qo'shimcha albom xaridlariga 15% chegirma</p>
-          </div>
-          <Button>Buy Package</Button>
-        </div>
-
-        <div className="w-full h-full border-2 border-red-600 rounded-md   bg-black p-8 text-white text-center">
-          <div className="flex text-4xl py-3 gap-4 flex-col justify-center items-center">
-            <ImHeadphones />
-            <h2>Debyut</h2>
-          </div>
-          <div className="flex items-center justify-between py-4">
-            <FaCheck className="text-red-600" />
-            <p>Yiliga 2 ta albom</p>
-          </div>
-          <hr />
-          <div className="flex items-center justify-between py-4 ">
-            <FaCheck className="text-red-600" />
-            <p>1 x ustuvor konsert chiptasi</p>
-          </div>
-          <hr />
-          <div className="flex items-center justify-between py-4 ">
-            <FaCheck className="text-red-600" />
-            <p>1 x bepul tovar mahsuloti</p>
-          </div>
-          <hr />
-          <div className="flex items-center justify-between py-4">
-            <FaCheck className="text-red-600" />
-            <p>Qo'shimcha albom xaridlariga 10% chegirma.</p>
+            <p>Qo'shimcha albom xaridlariga 20% chegirma</p>
           </div>
           <Button>Buy Package</Button>
         </div>
